35284528894a1d28d496128dde0ca1feb9cc95a3
gabogg07 <gabrieleduardogg@gmail.com>
2018-11-14 17:16:20 -0400

    Statement de ordenar e imprimir lista

---
+++ AniBot.pl
@@ -0,0 +1,32 @@
+anime(X) :- member(X,["Dragon Ball", "Naruto", "Bleach", "HunterXHunter", "Hamtaro", "Full Metal Alchemist"]).
+
+genero(X) :- member(X,["Aventura", "Shoujo", "Shounen", "Kodomo", "Seinen", "Josei", "Ficción",
+                    "Fantasía", "Mecha", "Sobrenatural", "Magia", "Gore"]).
+
+generoAnime("Naruto",["Shounen","Aventura"]).
+generoAnime("Dragon Ball",["Shounen"]).
+generoAnime("Bleach",["Shounen", "Sobrenatural"]).
+generoAnime("HunterXHunter",["Seinen", "Aventura"]).
+generoAnime("Hamtaro",["Kodomo"]).
+generoAnime("Full Metal Alchemist",["Shounen", "Magia"]).
+
+rating("Dragon Ball",3).
+rating("Naruto",1).
+rating("Bleach",4).
+rating("HunterXHunter",5).
+rating("Hamtaro",2).
+rating("Full Metal Alchemist",4).
+
+popularidad("Dragon Ball",7).
+popularidad("Naruto",5).
+popularidad("Bleach",8).
+popularidad("HunterXHunter",3).
+popularidad("Hamtaro",10).
+popularidad("Full Metal Alchemist",1).
+
+printByLine([]).
+printByLine([X|List]) :- writeln(X), printByLine(List).
+
+orderBy(rating) :- findall((Y,X),rating(Y,X), List), sort(2,  @>=, List,  Sorted), printByLine(Sorted).
+orderBy(popularidad) :- findall((Y,X),popularidad(Y,X), List), sort(2,  @>=, List,  Sorted), printByLine(Sorted).
+ 
