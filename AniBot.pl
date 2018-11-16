bcc674472453ecce379dcfbf1b62fb984989f1f0
gabogg07 <gabrieleduardogg@gmail.com>
2018-11-16 10:31:40 -0400

    Preguntas por rating 1-5 y caso error

--- AniBot.pl
+++ AniBot.pl
@@ -30,7 +30,7 @@ printByLine([X|List]) :- writeln(X), printByLine(List).
 
 
 printAnime([]).
-printAnime([(X,Y)|List]) :- writeln(X), printAnime(List).
+printAnime([(X,Y)|List]) :- atom_concat(" - ",X, Anime), writeln(Anime), printAnime(List).
 
 %Pasa una lista de strings todos a minuscula
 toLower([],[]).
@@ -43,10 +43,18 @@ orderBy(rating) :- findall((Y,X),rating(Y,X), List), sort(2,  @>=, List,  Sorted
 orderBy(popularidad) :- findall((Y,X),popularidad(Y,X), List), sort(2,  @>=, List,  Sorted), printByLine(Sorted).
  
 %Leer input el usuario y llama a la lista de respuestas
-leerRespuesta :- readln(X), nl, respuesta(X).
+leerRespuesta :- readln(X), nl, respuesta(X), leerRespuesta.
+
+%Verifica que un entero este entre 1 y X
+checkRange(X,Top) :- X @=< Top , 1 @=< X.
 
 %Queries sobre rating
 respuesta([cuales,son,los,mejores,rating,?]):- findall((X,_),rating(X,5), List), writeln("Listado de animes con rating 5: "), printAnime(List).
+respuesta([cuales,son,los,peores,rating,?]):- findall((X,_),rating(X,1), List), writeln("Listado de animes con rating 1: "), printAnime(List).
+respuesta([que,animes,tienen,rating,Q,?]):- checkRange(Q,5), findall((X,_),rating(X,Q), List), atom_concat("Listado de animes con rating ", Q, Salida), 
+										writeln(Salida), printAnime(List).
+respuesta([que,animes,tienen,rating,Q,?]):- writeln("Disculpa, pero solo me han hablado de ratings entre 1 y 5. Intenta con uno de estos valores.").
+
 
 
 %Queries sobre popularidad
